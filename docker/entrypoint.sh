2b957d1779d9fd9f2003df84140b3871920391fd
Soapy7261 Official <81933267+Soapy7261@users.noreply.github.com>
2024-12-27 20:33:22 -0500

    up to date

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/ash
-mysqld --datadir=/var/lib/mysql --skip-networking=0 --socket=/run/mysqld/mysqld.sock & npm start
+mariadbd --datadir=/var/lib/mysql --skip-networking=0 --socket=/run/mysqld/mysqld.sock & npm start
